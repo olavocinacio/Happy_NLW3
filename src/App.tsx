978afcf180d851acb223965ae3b6bd9667757ad4
Olavo <olavo1611@gmail.com>
2020-10-14 11:00:13 -0300

    Restauração do index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+function Title(){
+  return <h1>Hello NLW</h1>
+}
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Title />
     </div>
   );
 }

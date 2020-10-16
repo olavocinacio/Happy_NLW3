47a9d1b6d2e7e894aeb7e3989d92b0d1ce18d551
olavocinacio <olavo1611@gmail.com>
2020-10-15 23:36:02 -0300

    Criação de OrphanagesMap

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,32 @@
-import { ReactComponent } from "*.svg";
 import React from "react";
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+
+import mapMarkering from '../images/Logo_icon.svg';
+
+import '../styles/pages/OrphanagesMap.css';
 
 function OrphanagesMap(){
     return(
-        <h1>Hello world</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkering} alt="Happy"/>
+                    <h2>Escdolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita</p>
+                </header>
+
+                <footer>
+                    <strong>Luiz Antônio</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+            <div></div>
+
+            <Link to ='' className="create-orphanage">
+                <FiPlus size={32} color = '#fff'/>
+            </Link>
+        </div>
     );
 }
 

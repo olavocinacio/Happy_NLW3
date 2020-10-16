1947015e4586eb03124a626324e6f55fb5ba39fc
olavocinacio <olavo1611@gmail.com>
2020-10-16 00:19:26 -0300

    Adição do mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import {FiPlus} from 'react-icons/fi';
+import {Map, TileLayer} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css'
 
 import mapMarkering from '../images/Logo_icon.svg';
 
@@ -21,7 +24,11 @@ function OrphanagesMap(){
                     <span>São Paulo</span>
                 </footer>
             </aside>
-            <div></div>
+
+            <Map center={[-21.5491957,-47.6947521]} zoom={15} style={{width:'100%', height: '100%'}}>
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/> */}
+            </Map>
 
             <Link to ='' className="create-orphanage">
                 <FiPlus size={32} color = '#fff'/>

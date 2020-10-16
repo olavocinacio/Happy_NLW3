da69b08415f805dfd84bab04a1999a2e9eea940c
olavocinacio <olavo1611@gmail.com>
2020-10-15 23:14:10 -0300

    Criação do sistema de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import {FiArrowRight} from 'react-icons/fi'
 
 import "./styles/global.css";
-import "./styles/pages/landing.css";
 
-import logoImg from './images/Logo.svg';
+import Routes from './routes';
 
 // interface TitleProps{
 //   text: string;
@@ -16,25 +14,7 @@ import logoImg from './images/Logo.svg';
 
 function App() {
   return (
-    <div id = "page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
-          <main>
-            <h1>Leve felicidade para o mundo   </h1>
-            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-          </main>
-
-          <div className="location">
-            <strong>Luiz Antônio</strong>
-            <span>São Paulo</span>
-          </div>
-
-          <a href="" className="enter-app">
-            <FiArrowRight size={26} color="rgba{0,0,0,0.6}"/>
-          </a>
-      </div>
-    </div>
-
+    <Routes/>
   );
 }
 
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,10 @@
+import { ReactComponent } from "*.svg";
+import React from "react";
+
+function OrphanagesMap(){
+    return(
+        <h1>Hello world</h1>
+    );
+}
+
+export default OrphanagesMap;

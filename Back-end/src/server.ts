81b2e26c5350bb5c06f0df36e812ccdb66020713
olavocinacio <olavo1611@gmail.com>
2020-10-18 16:18:17 -0300

    Criação e conexão com banco de dados

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -1,5 +1,14 @@
 import express from 'express'; //Express auxilia no sistema de rotas baseando-se em requisições e respostas
 
-const app = express();
-app.listen(3333);
+import './database/connection';
 
+const app = express();
+
+app.use(express.json()); //Faz com que o express reconheça JSON
+
+//Rota = conjunto, recurso = usuário, Métodos HTTP, Parâmetro -> query params (filtro), route params (identificador), body (informações compostas)
+app.post('/users', (request, response) => {
+    return response.send({message:"Hello world"});
+});
+
+app.listen(3333);
